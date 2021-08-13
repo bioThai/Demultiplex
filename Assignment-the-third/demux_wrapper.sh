43960ac1ca2b8437414443632ed081ad9d1d9b63
Thaibinhduong Nguyen <tnguye14@talapas-ln1.cm.cluster>
2021-08-13 16:40:28 -0700

    modified wrapper filepaths

--- Assignment-the-third/demux_wrapper.sh
+++ Assignment-the-third/demux_wrapper.sh
@@ -3,13 +3,13 @@
 #SBATCH --account=bgmp
 #SBATCH --partition=bgmp
 #SBATCH --cpus-per-task=8
-#SBATCH --time=30:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=demux_wrapper%j.out
 #SBATCH --error=demux_wrapper%j.err
 
 conda activate bgmp_py39
 
-script_file="/projects/bgmp/tnguye14/bioinfo/Bi622/demultiplex_sandbox/part3/demux.py"
+script_file="/projects/bgmp/tnguye14/bioinfo/Bi622/Demultiplex/Assignment-the-third/demux.py"
 read1_file="/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz"
 read2_file="/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
 index1_file="/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz"
