1dbd5886954c4179e008750a58a2bc9b5d9ee517
Thaibinhduong Nguyen <tnguye14@talapas-ln1.cm.cluster>
2021-08-12 23:52:30 -0700

    add slurm wrapper scripts

---
+++ Assignment-the-first/read_qscores_R4_wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --cpus-per-task=8
+#SBATCH --time=16:00:00
+#SBATCH --output=r4_wrapper%j.out
+#SBATCH --error=r4_wrapper%j.err
+
+conda activate bgmp_py39
+script_file="/projects/bgmp/tnguye14/bioinfo/Bi622/demultiplex/part1/read_qscores.py"
+input_file="/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
+output_file_prefix="1294_S1_L008_R4_001.fastq.gz"
+
+/usr/bin/time -v python $script_file -f $input_file -o $output_file_prefix
+
+exit
